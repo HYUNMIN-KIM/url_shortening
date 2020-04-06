a587dfde573d0e84ec765e89c703a5c60ad6186f
HYUNMINKIM <khyunm91@gmail.com>
2020-04-06 18:29:23 +0900

    필요없는 import 삭제

--- src/test/java/com/example/url_shortening/controller/ShorteningControllerTest.java
+++ src/test/java/com/example/url_shortening/controller/ShorteningControllerTest.java
@@ -10,21 +10,21 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import org.springframework.test.web.servlet.ResultMatcher;
+
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.util.MultiValueMap;
+
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.mockito.ArgumentMatchers.contains;
+
 import static org.mockito.BDDMockito.given;
 
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
